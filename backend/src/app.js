608d462dec712edc92c1dbd0829ff8c919360dd3
Amanjeet Sharma <amansharma23503@gmail.com>
2025-08-21 15:42:59 +0530

    try

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import { errorHandler } from './middlewares/error.middleware.js';
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || 'https://work-in-progress-orcin.vercel.app',
     credentials: true
 }));
 
